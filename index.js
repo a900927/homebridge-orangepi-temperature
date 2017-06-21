f2356e3f7d95cc92774829af1edce70f9f20bb9e
CCZ <mmlsc@qq.com>
2017-06-21 10:32:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 var rf=require("fs");
 var Accessory, Service, Characteristic, UUIDGen;
 
+
 module.exports = function(homebridge) {
   Accessory = homebridge.platformAccessory;
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
   UUIDGen = homebridge.hap.uuid;
 
-  homebridge.registerAccessory('homebridge-orangepi-temperature', 'OrangePiTemperature', OrangePiTemperature);
+  homebridge.registerAccessory('homebridge-pi-temperature', 'PiTemperature', PiTemperature);
 }
 
-function OrangePiTemperature(log, config) {
+function PiTemperature(log, config, unit, piModel) {
   this.log = log;
   this.name = config["name"];
-  
+  this.unit = config["unit"];
+  this.piModel= config["piModel"];
+
 }
 
-OrangePiTemperature.prototype = {
+PiTemperature.prototype = {
   identify: function(callback) {
     callback();
   },
@@ -26,25 +29,25 @@ OrangePiTemperature.prototype = {
 
     var infoService = new Service.AccessoryInformation();
     infoService
-      .setCharacteristic(Characteristic.Manufacturer, "OrangePi")
-      .setCharacteristic(Characteristic.Model, "Zero")
+      .setCharacteristic(Characteristic.Manufacturer, "Pi")
+      .setCharacteristic(Characteristic.Model, this.piModel)
       .setCharacteristic(Characteristic.SerialNumber, "Orange");
     services.push(infoService);
 
-    var raspberrypiService = new Service.TemperatureSensor(this.name);
-    raspberrypiService
+    var piService = new Service.TemperatureSensor(this.name);
+    piService
       .getCharacteristic(Characteristic.CurrentTemperature)
       .on('get', this.getTemperature.bind(this));
-    services.push(raspberrypiService);
+    services.push(piService);
     
     return services;
   },
   
   getTemperature: function(callback) {
     var data = rf.readFileSync("/sys/class/thermal/thermal_zone0/temp","utf-8");
-    var temperature = parseFloat(data);
+    var temperature = parseFloat(data)/parseInt(this.unit); 
     this.log.debug("temperature: " + temperature);
-    
+    this.log(temperature +"/"+ parseInt(this.unit));
     callback(null, temperature);
   }
 
